f31a8112fef244b9eb745e51fd8f61173044a15a
Franklin Waller <franklin.dijks@gmail.com>
2020-01-15 19:49:20 +0100

    feat(pwa): Add minimum to be installable

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,25 +1,33 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import { Route, Redirect } from 'react-router';
-import { HashRouter } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
+import { Route, Redirect } from "react-router";
+import { HashRouter } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
 
 // Layouts
-import DefaultLayout from './layout/DefaultLayout';
+import DefaultLayout from "./layout/DefaultLayout";
 
-import store, { history } from './store';
-import LoadableStoreApp from './apps/Store';
+import store, { history } from "./store";
+import LoadableStoreApp from "./apps/Store";
 
 export default class App {
     domId: string;
 
-    constructor(id: string = 'app') {
+    constructor(id: string = "app") {
         this.domId = id;
     }
 
     run() {
+        // Check that service workers are supported
+        if ('serviceWorker' in navigator) {
+            // Use the window load event to keep the page load performant
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('./serviceWorker.js');
+            });
+        }
+
         ReactDOM.render(
             <Provider store={store}>
                 <ConnectedRouter history={history}>
@@ -32,8 +40,7 @@ export default class App {
                     </HashRouter>
                 </ConnectedRouter>
             </Provider>,
-            document.getElementById(this.domId),
+            document.getElementById(this.domId)
         );
     }
 }
-
